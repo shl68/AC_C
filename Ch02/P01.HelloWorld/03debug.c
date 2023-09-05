3e7caa8d882b95e4443ea4c319c544ab5836d845
shl68 <105649474+shl68@users.noreply.github.com>
2023-09-05 15:26:13 +0900

    Add files via upload

---
+++ Ch02/P01.HelloWorld/03debug.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+
+	printf("1\n");
+	printf("@\n");
+
+	return 0;
+}
---
+++ Ch02/P03.Debugging/P03.Debugging/03debug.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+
+	printf("1\n");
+	printf("2\n");
+
+	return 0;
+}
