43f1beb3036bea4a088b743ef58db553c1bb6c3f
shl68 <105649474+shl68@users.noreply.github.com>
2023-09-19 14:15:07 +0900

    Create PrintfHomeWork.c

---
+++ Ch03/PrintfHomeWork.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+
+	printf(" ^ * ! # @ \n");
+	printf("%f, %f, %f \n", 10.63, 2.34567E3, 2.34567E-3);
+	printf("\"%c\" 언어는 재미있는 \' 프로그래밍 언어 \' 이네요.", 67);
+
+		return 0;
+}
